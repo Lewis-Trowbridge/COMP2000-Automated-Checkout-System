8a2bf91e396e5120c751301c2f7b390be7b711d7
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-02 15:21:24 +0000

    Finished basic singleton pattern in Repository and added ObjectMapper import for later

--- src/com/dbaccess/Repository.java
+++ src/com/dbaccess/Repository.java
@@ -1,5 +1,6 @@
 package com.dbaccess;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Repository {
     private static Repository repositoryInstance;
@@ -18,6 +19,9 @@ public class Repository {
         // Automatically generated method. Please delete this comment before entering specific code.
 //end of modifiable zone..................E/a5b7573e-ec82-4f8a-a475-f37ea927c07d
 //begin of modifiable zone................T/16ab4d42-20dd-4262-9dc7-7bd2403a12cc
+        if (repositoryInstance == null){
+            repositoryInstance = new Repository();
+        }
         return repositoryInstance;
 //end of modifiable zone..................E/16ab4d42-20dd-4262-9dc7-7bd2403a12cc
     }
