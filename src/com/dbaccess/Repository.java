c2f326639626a7b9fec5d00869604d9f2663c2f7
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-02 12:15:01 +0000

    Initial commit with skeleton repository class and project structure

---
+++ src/com/dbaccess/Repository.java
@@ -0,0 +1,25 @@
+package com.dbaccess;
+
+
+public class Repository {
+    private static Repository repositoryInstance;
+
+    private Repository() {
+//begin of modifiable zone(JavaSuper).....C/190b61f6-ae29-4a9a-9053-baf0f262fe57
+
+//end of modifiable zone(JavaSuper).......E/190b61f6-ae29-4a9a-9053-baf0f262fe57
+//begin of modifiable zone(JavaCode)......C/190b61f6-ae29-4a9a-9053-baf0f262fe57
+
+//end of modifiable zone(JavaCode)........E/190b61f6-ae29-4a9a-9053-baf0f262fe57
+    }
+
+    public static Repository getRepositoryInstance() {
+//begin of modifiable zone................T/a5b7573e-ec82-4f8a-a475-f37ea927c07d
+        // Automatically generated method. Please delete this comment before entering specific code.
+//end of modifiable zone..................E/a5b7573e-ec82-4f8a-a475-f37ea927c07d
+//begin of modifiable zone................T/16ab4d42-20dd-4262-9dc7-7bd2403a12cc
+        return repositoryInstance;
+//end of modifiable zone..................E/16ab4d42-20dd-4262-9dc7-7bd2403a12cc
+    }
+
+}
