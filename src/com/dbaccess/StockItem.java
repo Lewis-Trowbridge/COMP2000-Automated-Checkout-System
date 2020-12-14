b052bdf73cbebeaf1d953d7cc2bc5b8c49968c28
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-14 11:26:02 +0000

    Added annotations to allow for JSON conversion and equals method

--- src/com/dbaccess/StockItem.java
+++ src/com/dbaccess/StockItem.java
@@ -1,6 +1,8 @@
 package com.dbaccess;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StockItem {
     private int itemId;
 
@@ -8,6 +10,7 @@ public class StockItem {
 
     private float itemPrice;
 
+    @JsonProperty("itemPrice")
     float getItemPrice() {
 //begin of modifiable zone................T/50f71d57-5e73-4879-9391-f426287cd6a0
         // Automatically generated method. Please delete this comment before entering specific code.
@@ -26,6 +29,7 @@ public class StockItem {
 //end of modifiable zone..................E/9a23d36d-71b6-4fb6-ba24-d9d077e42980
     }
 
+    @JsonProperty("itemId")
     int getItemId() {
 //begin of modifiable zone................T/15287f22-1fc1-4a62-8978-66df649fa04d
         // Automatically generated method. Please delete this comment before entering specific code.
@@ -42,6 +46,7 @@ public class StockItem {
 //end of modifiable zone..................E/308c68c8-047f-41a7-83c1-ab943b1e8d50
     }
 
+    @JsonProperty("itemName")
     String getItemName() {
 //begin of modifiable zone................T/5e73a64b-ca3e-4116-8450-7a9e152b006c
         // Automatically generated method. Please delete this comment before entering specific code.
@@ -56,6 +61,25 @@ public class StockItem {
         // Automatically generated method. Please delete this comment before entering specific code.
         this.itemName = value;
 //end of modifiable zone..................E/0d907bba-e152-4fd7-80b8-83378bb20dff
+    }
+
+    @Override
+    public boolean equals(Object o) {
+//begin of modifiable zone(JavaCode)......C/40652c1c-6683-41ec-be34-b9c80d8f4113
+        if (o == this){
+            return true;
+        }
+
+        if (!(o instanceof StockItem)){
+            return false;
+        }
+
+        StockItem newItem = (StockItem)o;
+
+        return this.itemId == newItem.itemId && this.itemName.equals(newItem.itemName) && this.itemPrice == newItem.itemPrice;
+//end of modifiable zone(JavaCode)........E/40652c1c-6683-41ec-be34-b9c80d8f4113
+//begin of modifiable zone(JavaReturned)..C/40652c1c-6683-41ec-be34-b9c80d8f4113
+//end of modifiable zone(JavaReturned)....E/40652c1c-6683-41ec-be34-b9c80d8f4113
     }
 
 }
