190daf910091e1514419a8a1682b6724234dbb4e
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-17 20:02:50 +0000

    Added title and placeholder panel to main frame

--- src/com/autochecksys/gui/shared/MainFrame.java
+++ src/com/autochecksys/gui/shared/MainFrame.java
@@ -6,12 +6,17 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     private JPanel topLevelPanel;
+    private JLabel lblTitle;
+    private JPanel pnlLoginHolder;
 
     public MainFrame(){
         setContentPane(topLevelPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(500, 500));
+        setUpComponents();
         pack();
     }
 
+    private void setUpComponents(){
+    }
 }
