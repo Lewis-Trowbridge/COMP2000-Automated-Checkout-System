2c486386a0992e98cf0a90276657a421d6a94ab7
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-22 16:28:15 +0000

    Updated DisplayPanel to move setup from constructor to avoid calling abstract method from constructor

--- src/com/autochecksys/gui/shared/DisplayPanel.java
+++ src/com/autochecksys/gui/shared/DisplayPanel.java
@@ -11,8 +11,6 @@ public abstract class DisplayPanel extends JPanel {
 //end of modifiable zone(JavaSuper).......E/91cfef97-d38e-4483-b34e-2f3628e871b5
 //begin of modifiable zone................T/3a482217-5049-4e7e-96d3-fbf859f563f8
         this.mainFrameToUse = frameToDisplayIn;
-        setUpComponents();
-        display();
 //end of modifiable zone..................E/3a482217-5049-4e7e-96d3-fbf859f563f8
     }
 
@@ -22,6 +20,13 @@ public abstract class DisplayPanel extends JPanel {
 //begin of modifiable zone................T/07ee780b-ad62-4872-a680-1adafc08e988
         mainFrameToUse.displayPanel(this);
 //end of modifiable zone..................E/07ee780b-ad62-4872-a680-1adafc08e988
+    }
+
+    public void setupAndDisplay() {
+//begin of modifiable zone(JavaCode)......C/808f9c52-0b5b-4f09-8ad5-fce3c1be7d76
+        setUpComponents();
+        display();
+//end of modifiable zone(JavaCode)........E/808f9c52-0b5b-4f09-8ad5-fce3c1be7d76
     }
 
 }
