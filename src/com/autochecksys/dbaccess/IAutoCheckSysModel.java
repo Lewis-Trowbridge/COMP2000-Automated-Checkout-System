91b06a8e9131ca8073bd130d108d549a24f0625f
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-26 15:34:03 +0000

    Added IAutoCheckSysModel interface to allow for extendable usage of models in controller classes and the implement observer pattern

---
+++ src/com/autochecksys/dbaccess/IAutoCheckSysModel.java
@@ -0,0 +1,13 @@
+package com.autochecksys.dbaccess;
+
+import com.autochecksys.KeyValuePair;
+import com.autochecksys.controller.shared.AbstractController;
+
+public interface IAutoCheckSysModel {
+    void add(AbstractController observer);
+
+    void remove(AbstractController observer);
+
+    void onPropertyChanged(KeyValuePair change);
+
+}
