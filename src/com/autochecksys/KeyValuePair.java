35679fa9b5290e35572c7d412c2a728682b2c3f9
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-26 15:24:06 +0000

    Added KeyValuePair class for purposes of transmission of data between classes

---
+++ src/com/autochecksys/KeyValuePair.java
@@ -0,0 +1,19 @@
+package com.autochecksys;
+
+
+public class KeyValuePair {
+    public String key;
+
+    public Object value;
+
+    public KeyValuePair(String key, Object value) {
+//begin of modifiable zone(JavaSuper).....C/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+
+//end of modifiable zone(JavaSuper).......E/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+//begin of modifiable zone(JavaCode)......C/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+        this.key = key;
+        this.value = value;
+//end of modifiable zone(JavaCode)........E/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+    }
+
+}
