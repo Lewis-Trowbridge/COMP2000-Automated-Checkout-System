8ce9c8aceeb4b91d642b395ea928315f6c46d549
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-29 14:18:05 +0000

    Added text field and button to accept barcodes in KioskPanel

--- src/com/autochecksys/view/kiosk/KioskPanel.java
+++ src/com/autochecksys/view/kiosk/KioskPanel.java
@@ -1,12 +1,20 @@
 package com.autochecksys.view.kiosk;
 
+import java.awt.*;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import com.autochecksys.KeyValuePair;
 import com.autochecksys.view.shared.DisplayPanel;
+import com.autochecksys.view.shared.GridBagConstraintsBuilder;
 import com.autochecksys.view.shared.MainFrame;
 
 public class KioskPanel extends DisplayPanel {
     public JLabel lblKiosk;
+
+    public JTextField txfBarcodeInput;
+
+    public JButton btnBarcodeEnter;
 
     public KioskPanel(MainFrame mainFrameToUse) {
 //begin of modifiable zone................T/659be782-e128-4838-a1d4-2cacf7025a79
@@ -19,10 +27,21 @@ super(mainFrameToUse);
     }
 
     @Override
-    public void setUpComponents() {
+    protected void setUpComponents() {
 //begin of modifiable zone................T/32ed2544-31bb-4081-86f0-64ff77155ffc
+        this.setLayout(new GridBagLayout());
+        GridBagConstraintsBuilder builder = new GridBagConstraintsBuilder();
         lblKiosk = new JLabel("Test");
-        this.add(lblKiosk);
+        GridBagConstraints lblKioskConstraints = builder.setGridWidth(4).build();
+        txfBarcodeInput = new JTextField();
+        GridBagConstraints txfBarcodeInputConstraints = builder.setGridY(1).setGridWidth(3).setFill(GridBagConstraints.HORIZONTAL).build();
+        btnBarcodeEnter = new JButton("Go");
+        GridBagConstraints btnBarcodeEnterConstraints = builder.setGridX(3).setGridY(1).setFill(GridBagConstraints.HORIZONTAL).build();
+        
+        
+        this.add(lblKiosk, lblKioskConstraints);
+        this.add(txfBarcodeInput, txfBarcodeInputConstraints);
+        this.add(btnBarcodeEnter, btnBarcodeEnterConstraints);
 //end of modifiable zone..................E/32ed2544-31bb-4081-86f0-64ff77155ffc
     }
 
