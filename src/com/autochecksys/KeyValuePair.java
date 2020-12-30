9283047e5e324213e0267b1bab9e7c909284f158
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-30 12:32:24 +0000

    Updated KeyValuePair to include an ID in the case that an ID-linked value is updated and needs to be referenced by the receiver

--- src/com/autochecksys/KeyValuePair.java
+++ src/com/autochecksys/KeyValuePair.java
@@ -6,14 +6,17 @@ public class KeyValuePair {
 
     public Object value;
 
-    public KeyValuePair(String key, Object value) {
+    public int id;
+
+    public KeyValuePair(String key, Object value, int id) {
 //begin of modifiable zone(JavaSuper).....C/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
 
 //end of modifiable zone(JavaSuper).......E/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
-//begin of modifiable zone(JavaCode)......C/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+//begin of modifiable zone................T/e241943f-b06b-41e8-ac03-0c54ed3f7d61
         this.key = key;
         this.value = value;
-//end of modifiable zone(JavaCode)........E/fe10d3ae-1a01-476c-aab6-9e9c69bf719f
+        this.id = id;
+//end of modifiable zone..................E/e241943f-b06b-41e8-ac03-0c54ed3f7d61
     }
 
 }
